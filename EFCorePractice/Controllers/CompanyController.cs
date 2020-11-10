c498f292e0804ab184592589c9f5a27b4c1005b8
ChenXu <xuchen@thoughtworks.com>
2020-11-10 22:38:27 +0800

    feat: complete get/post apis

--- EFCorePractice/Controllers/CompanyController.cs
+++ EFCorePractice/Controllers/CompanyController.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using EFCorePractice.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,16 +12,31 @@ namespace EFCorePractice.Controllers
     [Route("companies")]
     public class CompanyController : ControllerBase
     {
+        private readonly CompanyDbContext companyDbContext;
+
+        public CompanyController(CompanyDbContext companyDbContext)
+        {
+            this.companyDbContext = companyDbContext;
+        }
+
         [HttpGet]
         public async Task<IEnumerable<Company>> List()
         {
-            return null;
+            await using (companyDbContext)
+            {
+                return await companyDbContext.Companies.ToListAsync();
+            }
         }
 
         [HttpPost]
         public async Task<Company> Add(Company company)
         {
-            return null;
+            await using (companyDbContext)
+            {
+                await companyDbContext.Companies.AddAsync(company);
+                await companyDbContext.SaveChangesAsync();
+                return company;
+            }
         }
     }
 }
